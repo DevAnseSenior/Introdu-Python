ddfff32623e16489c73724cae2fc8d41d6f37be0
darlan kennedy <darlankennedy2@gmail.com>
2019-08-11 12:48:53 -0300

    atualização

---
+++ Exemplos/teste_darlan_1.py
@@ -0,0 +1,7 @@
+nome = input("Digite Seu Nome  : ")
+idade = input("Digite sua idade: ")
+peso =input("Digite seu peso   :")
+print("Meu nome:"+nome)
+print("Minha Idade:"+idade)
+print("Meu peso:"+peso)
+
